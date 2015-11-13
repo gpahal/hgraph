387455fab3093d450f64932c05fdbe2ee6262623
Garvit Pahal <g10pahal@gmail.com>
2015-11-13 14:49:07 +0530

    update Bench.hs

--- src/Bench.hs
+++ src/Bench.hs
@@ -8,14 +8,13 @@ import           SocialNetwork
 
 benchmarks :: Int -> Int -> Int -> Int -> [Benchmark]
 benchmarks u p f l =
-    let g = execGraph (createRandomGraph u p f l) initialGraph in
-    [ bench "creation"  (nf (const $ labelMaps g) ())
-    , bench "traversal" (nf (const $ length $ evalGraph (shortestPath 3 u DOUT ((>(u+p+1)) . fromInt . nodeId) [friendLabel] 1) g) ())
+    [ bench "creation"  (nf (const $ labelMaps $ execGraph (createRandomGraph u p f l) initialGraph) ())
+    , bench "traversal" (nf (const $ length $ evalGraph (shortestPath 3 u DOUT ((>(u+p+1)) . fromInt . nodeId) [friendLabel] 1) $ execGraph (createRandomGraph u p f l) initialGraph) ())
     ]
 
 main :: IO ()
 main = defaultMain
     [ bgroup "50" (benchmarks 50 30 3 2)
-    , bgroup "100" (benchmarks 100 80 4 2)
-    , bgroup "200" (benchmarks 200 150 6 3)
+    , bgroup "500" (benchmarks 500 400 8 4)
+    , bgroup "2000" (benchmarks 2000 1500 12 5)
     ]
